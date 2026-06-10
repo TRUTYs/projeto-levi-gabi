34c3d9dfe206374d732e30bc3e632ad290de949e
Levi1887 <levigomes282@gmail.com>
2026-06-09 22:08:18 -0300

    Delete Faculdade directory

--- Faculdade/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from .usuario     import Usuario
-from .fornecedor  import Fornecedor
-from .produto     import Produto
-from .movimentacao import Movimentacao
-__all__ = ["Usuario","Fornecedor","Produto","Movimentacao"]
--- Faculdade/services/__init__.py
+++
@@ -1,4 +0,0 @@
-from .estoque_service   import EstoqueService
-from .fornecedor_service import FornecedorService
-from .dashboard_service import DashboardService
-__all__ = ["EstoqueService","FornecedorService","DashboardService"]
