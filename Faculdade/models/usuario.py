58b52c6dcef49c44ab172603749a6f7a09abca98
Levi1887 <levigomes282@gmail.com>
2026-06-11 22:00:17 -0300

    Delete Faculdade directory

--- Faculdade/models/usuario.py
+++
@@ -1,8 +0,0 @@
-from extensions import db
-class Usuario(db.Model):
-    __tablename__ = "usuario"
-    id    = db.Column(db.Integer, primary_key=True)
-    nome  = db.Column(db.String(50), nullable=False, unique=True)
-    email = db.Column(db.String(100), unique=True)
-    senha = db.Column(db.String(200), nullable=False)
-    tipo  = db.Column(db.Enum("admin","vendedor"), nullable=False, default="vendedor")
