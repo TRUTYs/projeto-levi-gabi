1e9f7ec16f0b7566a2ac46043a75bfa9138cb16e
Levi1887 <levigomes282@gmail.com>
2026-06-15 21:39:58 -0300

    Delete Faculdade directory

--- Faculdade/config.py
+++
@@ -1,7 +0,0 @@
-import os
-BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-
-class Config:
-    SECRET_KEY = os.environ.get("SECRET_KEY", "troque-em-producao")
-    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(BASE_DIR, "projeto.db")
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
